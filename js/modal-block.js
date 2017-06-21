3a4dc57fe0ea390cc2ef83920d3e01e59b12b582
Alexandra Drapash <katzmeneli@gmail.com>
2017-06-21 23:28:00 +0300

    Form changes

--- js/modal-block.js
+++ js/modal-block.js
@@ -2,14 +2,14 @@ var order = document.querySelector(".goods__order");
 var popup = document.querySelector(".modal-block");
 var popupClose = popup.querySelector(".modal-block--closed");
 
-if (order){
-order.addEventListener("click", function(event) {
-	var target = event.target;
-	event.preventDefault();
-		if (target.classList.contains("goods__btn")) {
+if (order) {
+  order.addEventListener("click", function(event) {
+    var target = event.target;
+    event.preventDefault();
+    if (target.classList.contains("goods__btn")) {
       popup.classList.remove("modal-block--closed");
-	    popup.classList.add("modal-block--show");
-		}
+      popup.classList.add("modal-block--show");
+    }
 	});
 }
 
