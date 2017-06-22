c4b6c3d83cfb376a32adb18d4782f9b6dc63b7d6
Alexandra Drapash <katzmeneli@gmail.com>
2017-06-22 16:56:58 +0300

    Svg added, overlay

--- js/modal-block.js
+++ js/modal-block.js
@@ -1,6 +1,7 @@
 var order = document.querySelector(".goods__order");
 var popup = document.querySelector(".modal-block");
 var popupClose = popup.querySelector(".modal-block--closed");
+var overlay = document.querySelector(".overlay");
 
 if (order) {
   order.addEventListener("click", function(event) {
@@ -9,14 +10,19 @@ if (order) {
     if (target.classList.contains("goods__btn")) {
       popup.classList.remove("modal-block--closed");
       popup.classList.add("modal-block--show");
+      overlay.classList.remove("overlay--close");
+      overlay.classList.add("overlay--show");
     }
-	});
+  });
 }
 
 window.addEventListener("keydown", function(event) {
 	if (event.keyCode === 27) {
 		if (popup.classList.contains("modal-block--show")) {
 			popup.classList.remove("modal-block--show");
+		}
+    if (overlay.classList.contains("overlay--show")) {
+			overlay.classList.remove("overlay--show");
 		}
 	}
 });
